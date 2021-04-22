1e52edf7ef06c73c69fec9a264bad31831f454ad
Sean Dodson <dodsonsean@comcast.net>
2021-04-21 22:03:07 -0400

    Add topic resource population

--- src/api/src/graphql/resolvers.js
+++ src/api/src/graphql/resolvers.js
@@ -3,7 +3,25 @@ const { Resource } = require('../mongoose/models/resource');
 
 const resolvers = {
   Query: {
-    topics: () => Topic.find({}),
+    topics: async () => {
+      const topics = await Topic.find({}).populate('resources');
+
+      return topics.map((t) => {
+        return {
+          id: t._id,
+          name: t.name,
+          resources: t.resources.map((r) => {
+            return {
+              resource: {
+                id: r._id,
+                name: r.name,
+                link: r.link,
+              },
+            };
+          }),
+        };
+      });
+    },
     resources: () => Resource.find({}),
   },
   Mutation: {
