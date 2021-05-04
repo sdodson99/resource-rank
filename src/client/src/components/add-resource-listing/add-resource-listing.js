75858240552da241053e45b12b1f65865bb98120
Sean Dodson <dodsonsean@comcast.net>
2021-05-03 20:19:05 -0400

    Sort available resources

--- src/client/src/components/add-resource-listing/add-resource-listing.js
+++ src/client/src/components/add-resource-listing/add-resource-listing.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import AddResourceListingItem from '../add-resource-listing-item/add-resource-listing-item';
 
 function AddResourceListing({ availableResources, onAddResource }) {
-  const addResourceListingItems = availableResources.map((r) => (
-    <div key={r.id}>
-      <AddResourceListingItem
-        availableResource={r}
-        onAddResource={onAddResource}
-      />
-    </div>
-  ));
+  const addResourceListingItems = availableResources
+    .sort((r1, r2) => r1.alreadyAdded - r2.alreadyAdded)
+    .map((r) => (
+      <div key={r.id}>
+        <AddResourceListingItem
+          availableResource={r}
+          onAddResource={onAddResource}
+        />
+      </div>
+    ));
 
   return addResourceListingItems;
 }
