acd959b93543dea61bdd576b55f1f2338588eae7
Sean Dodson <dodsonsean@comcast.net>
2021-05-03 20:21:35 -0400

    Show already added resources

--- src/client/src/components/add-resource-listing-item/add-resource-listing-item.js
+++ src/client/src/components/add-resource-listing-item/add-resource-listing-item.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ListingItem from '../listing-item/listing-item';
+import { Plus } from 'react-bootstrap-icons';
 
 function AddResourceListingItem({ availableResource, onAddResource }) {
   const addResource = () => onAddResource(availableResource.id);
 
+  const { alreadyAdded } = availableResource;
+  const getNameDisplayClass = () => {
+    if (alreadyAdded) {
+      return 'text-decoration-line-through';
+    }
+
+    return '';
+  };
+
   return (
     <ListingItem>
-      <div className="row align-items-center justify-content-between">
-        <div className="col-sm-auto">{availableResource.name}</div>
+      <div className="row align-items-center justify-content-between text-center text-sm-start">
         <div className="col-sm-auto">
-          <button className="btn btn-primary" onClick={addResource}>
-            Add
+          <div>
+            <div className={getNameDisplayClass()}>
+              {availableResource.name}
+            </div>
+            {alreadyAdded && (
+              <div className="fst-italic fs-6 text-danger">Already added</div>
+            )}
+          </div>
+        </div>
+        <div className="col-sm-auto mt-3 mt-sm-0">
+          <button
+            className="btn btn-primary"
+            onClick={addResource}
+            disabled={alreadyAdded}
+          >
+            <Plus fill="white" width="25" height="25" />
           </button>
         </div>
       </div>
