8f07e90b94390dc2d94234f019c962500987da9d
Sean Dodson <dodsonsean@comcast.net>
2021-05-13 22:17:53 -0400

    Update connection string name

--- src/api/src/functions/index.js
+++ src/api/src/functions/index.js
@@ -2,7 +2,7 @@ const { https, config } = require('firebase-functions');
 const { createGQLServer } = require('../graphql/create-gql-server');
 const openMongoConnection = require('../mongoose/open-connection');
 
-const connectionString = config().mongo.connectionString;
+const connectionString = config().mongo.connection_string;
 openMongoConnection(connectionString);
 
 const gqlServer = createGQLServer();
