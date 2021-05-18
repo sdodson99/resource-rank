9f1168fde0edbb455a624ae96d85da1d3877a402
Sean Dodson <dodsonsean@comcast.net>
2021-05-18 10:01:44 -0400

    RR-77: Add 404 page

---
+++ src/client/src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layouts/layout';
+
+function _404_() {
+  return (
+    <Layout>
+      <div className="mt-5 page-header text-center">Uh-oh! Page not found.</div>
+    </Layout>
+  );
+}
+
+export default _404_;
