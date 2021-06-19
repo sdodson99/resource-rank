a006e8a47936eb51ad9d8849514b20a88e0d822b
Sean Dodson <dodsonsean@comcast.net>
2021-06-18 21:46:34 -0400

    RR-88: Move old client to NextJS client

--- src/client/pages/_app.js
+++ src/client/pages/_app.js
@@ -1,13 +1,43 @@
 import '../styles/globals.css';
 import PropTypes from 'prop-types';
+import { AuthenticationProvider } from './hooks/authentication/use-authentication-state';
+import { FirebaseAppProvider } from './hooks/use-firebase-app';
+import AuthenticationApolloProvider from '../components/authentication-apollo-provider';
+import '@fontsource/magra';
+import '@fontsource/magra/700.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const firebaseConfig = {
+  apiKey: 'AIzaSyChFnYmkhARBy0Hwtehlx-81rSC7PZZWT8',
+  authDomain: 'resource-rank.firebaseapp.com',
+  databaseURL: 'https://resource-rank-default-rtdb.firebaseio.com',
+  projectId: 'resource-rank',
+  storageBucket: 'resource-rank.appspot.com',
+  messagingSenderId: '489304913015',
+  appId: '1:489304913015:web:71f38a965ccde83db54272',
+  measurementId: 'G-D18DVJ4168',
+};
+const useAuthenticationEmulator =
+  process.env.USE_AUTHENTICATION_EMULATOR === 'true';
+const apolloUri = process.env.GQL_URI;
+
+function App({ Component, pageProps }) {
+  return (
+    <FirebaseAppProvider
+      firebaseConfig={firebaseConfig}
+      useAuthenticationEmulator={useAuthenticationEmulator}
+    >
+      <AuthenticationProvider>
+        <AuthenticationApolloProvider apolloUri={apolloUri}>
+          <Component {...pageProps} />
+        </AuthenticationApolloProvider>
+      </AuthenticationProvider>
+    </FirebaseAppProvider>
+  );
 }
 
-MyApp.propTypes = {
+App.propTypes = {
   Component: PropTypes.func,
   pageProps: PropTypes.object,
 };
 
-export default MyApp;
+export default App;
