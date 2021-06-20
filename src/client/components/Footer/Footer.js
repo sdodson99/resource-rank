ab26fe604082221078d59679576cdd0cdfce2e7d
Sean Dodson <dodsonsean@comcast.net>
2021-06-20 14:58:10 -0400

    RR-102: Cleanup footer styling

--- src/client/components/Footer/Footer.js
+++ src/client/components/Footer/Footer.js
@@ -2,38 +2,38 @@ import React from 'react';
 import styles from './Footer.module.css';
 import Image from 'next/image';
 
-const Footer = () => {
-  return (
-    <footer className={styles.Footer} data-testid="Footer">
-      <div>
-        <div>
-          <div>
-            <h1>Our Mission</h1>
-            <p>
-              is to <strong>simplify</strong> your learning journey. Learning
-              can be <strong>difficult</strong>, especially when starting. There
-              are <strong>millions</strong> of educational courses, videos, and
-              books <strong>scattered</strong> across the Internet.
-            </p>
-            <p>
-              Resource Rank’s goal is to <strong>consolidate</strong> all of
-              these resources so that you can <strong>quickly</strong> find the{' '}
-              <strong>best</strong> resources for what you are learning.
-            </p>
-          </div>
-          <div>
-            <Image
-              src="/img/logo.svg"
-              alt="Resource Rank Logo"
-              height="75"
-              width="75"
-            />
-          </div>
+const Footer = () => (
+  <footer className={styles.Footer} data-testid="Footer">
+    <div className="content-container">
+      <div className="sm:flex text-center sm:text-left">
+        <div className="order-1 sm:order-2 sm:ml-10">
+          <h1 className="text-5xl">Our Mission</h1>
+          <p className="py-3">
+            is to <strong>simplify</strong> your learning journey. Learning can
+            be <strong>difficult</strong>, especially when starting. There are{' '}
+            <strong>millions</strong> of educational courses, videos, and books{' '}
+            <strong>scattered</strong> across the Internet.
+          </p>
+          <p className="py-3">
+            Resource Rank’s goal is to <strong>consolidate</strong> all of these
+            resources so that you can <strong>quickly</strong> find the{' '}
+            <strong>best</strong> resources for what you are learning.
+          </p>
+        </div>
+        <div className="order-2 sm:order-1 mt-10 sm:mt-0">
+          <Image
+            src="/img/logo.svg"
+            alt="Resource Rank Logo"
+            height="75"
+            width="75"
+            layout="fixed"
+            priority={true}
+          />
         </div>
       </div>
-    </footer>
-  );
-};
+    </div>
+  </footer>
+);
 
 Footer.propTypes = {};
 
--- src/client/components/Header/Header.js
+++ src/client/components/Header/Header.js
@@ -63,6 +63,7 @@ const Header = () => {
                   alt="Resource Rank Logo"
                   height="50"
                   width="50"
+                  layout="fixed"
                   priority={true}
                 />
               </a>
