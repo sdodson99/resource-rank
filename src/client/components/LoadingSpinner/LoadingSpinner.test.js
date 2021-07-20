8b7f31cca5d80866a234923b15b46b54202addfe
Sean Dodson <dodsonsean@comcast.net>
2021-07-19 22:30:15 -0400

    RR-110: Add loading spinner

---
+++ src/client/components/LoadingSpinner/LoadingSpinner.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import LoadingSpinner from './LoadingSpinner';
+import renderer from 'react-test-renderer';
+
+describe('<LoadingSpinner />', () => {
+  test('it should mount', () => {
+    render(<LoadingSpinner />);
+
+    const loadingSpinner = screen.getByTestId('LoadingSpinner');
+
+    expect(loadingSpinner).toBeInTheDocument();
+  });
+
+  it('should render correctly', () => {
+    const tree = renderer.create(<LoadingSpinner />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
