7ebde24ffc8171d766b8ddb8441bfab2e8f1e700
Sean Dodson <dodsonsean@comcast.net>
2021-07-22 23:30:09 -0400

    RR-123: Add new topic page functionality

--- src/client/pages/topics/new.js
+++ src/client/pages/topics/new.js
@@ -3,12 +3,12 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import BreadcrumbLayout from '../../components/BreadcrumbLayout/BreadcrumbLayout';
 import Head from 'next/head';
-import useTopicExistsQuery from '../../hooks/use-topic-exists-query';
 import { useForm } from 'react-hook-form';
 import useCreateTopicMutation from '../../hooks/use-create-topic-mutation';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
 const FormField = {
-  NAME: 'name',
+  TOPIC_NAME: 'name',
 };
 
 export default function NewTopic() {
@@ -16,26 +16,69 @@ export default function NewTopic() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm({
     mode: 'onBlur',
     reValidateMode: 'onChange',
+    defaultValues: {
+      [FormField.TOPIC_NAME]: '',
+    },
   });
 
   const [createTopicError, setCreateTopicError] = useState();
 
-  const { execute: executeTopicExistsQuery } = useTopicExistsQuery();
-  const { execute: executeCreateTopicMutation } = useCreateTopicMutation();
+  const { execute: executeCreateTopicMutation, isLoading: isCreatingTopic } =
+    useCreateTopicMutation();
 
-  const onSubmit = async (formData) => {
-    const name = formData[FormField.NAME]
-    
-    console.log(name);
-    // await executeCreateTopicMutation(name);
-    // router.push('/topics');
+  const isTopicAlreadyExistsError = (error) => {
+    const errors = error.response?.errors;
+
+    if (!Array.isArray(errors)) {
+      return false;
+    }
+
+    if (errors.length === 0) {
+      return false;
+    }
+
+    const errorCode = errors[0].extensions?.code;
+
+    return errorCode === 'TOPIC_ALREADY_EXISTS';
+
+    if (errorCode === 'TOPIC_ALREADY_EXISTS') {
+    }
   };
 
-  const nameError = errors[FormField.NAME]?.message;
+  const onSubmit = async (formData) => {
+    setCreateTopicError(null);
+
+    const name = formData[FormField.TOPIC_NAME];
+
+    const { data, error } = await executeCreateTopicMutation(name);
+
+    if (error) {
+      if (isTopicAlreadyExistsError(error)) {
+        return setError(FormField.TOPIC_NAME, {
+          message: 'Name already exists.',
+        });
+      } else {
+        return setCreateTopicError(error);
+      }
+    }
+
+    const createdTopicId = data?.createTopic?.id;
+
+    if (!createdTopicId) {
+      return setCreateTopicError(new Error('Failed to create topic.'));
+    }
+
+    router.push(`/topics/${createdTopicId}`);
+  };
+
+  const onInvalid = () => setCreateTopicError(null);
+
+  const nameError = errors[FormField.TOPIC_NAME]?.message;
 
   const breadcrumbs = [
     {
@@ -56,13 +99,13 @@ export default function NewTopic() {
 
       <div className="text-4xl">New Topic</div>
 
-      <form className="mt-10" onSubmit={handleSubmit(onSubmit)}>
+      <form className="mt-10" onSubmit={handleSubmit(onSubmit, onInvalid)}>
         <div className="flex flex-col">
-          <label htmlFor={FormField.NAME}>Name</label>
+          <label htmlFor={FormField.TOPIC_NAME}>Topic Name</label>
           <input
             className="mt-2 form-control flex-grow"
             type="text"
-            {...register(FormField.NAME, {
+            {...register(FormField.TOPIC_NAME, {
               required: 'Required',
             })}
           />
@@ -73,17 +116,29 @@ export default function NewTopic() {
         </div>
 
         <div className="mt-10 flex flex-col sm:flex-row">
-          <button className="btn btn-primary w-100" type="submit">
+          <button
+            className="btn btn-primary w-100"
+            type="submit"
+            disabled={isCreatingTopic}
+          >
             Submit
           </button>
-          <button className="mt-3 btn btn-danger-outline w-100 sm:mt-0 sm:ml-3">
-            <Link href="/topics">Cancel</Link>
-          </button>
+          <Link href="/topics">
+            <a className="mt-3 text-center btn btn-danger-outline w-100 sm:mt-0 sm:ml-3">
+              Cancel
+            </a>
+          </Link>
+
+          {isCreatingTopic && (
+            <div className="mt-5 sm:mt-0 sm:ml-3 self-center">
+              <LoadingSpinner height={30} width={30} />
+            </div>
+          )}
         </div>
 
-        <div className="text-center text-sm-start">
+        <div className="text-center sm:text-left">
           {createTopicError && (
-            <div className="mt-4 text-danger">Failed to create topic.</div>
+            <div className="mt-6 error-text">Failed to create topic.</div>
           )}
         </div>
       </form>
