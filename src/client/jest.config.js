930a0007f3524c225f2ee7244b24a691013807eb
Sean Dodson <dodsonsean@comcast.net>
2021-07-25 15:43:36 -0400

    RR-129: Setup Jest coverage

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -4,4 +4,20 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
   },
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: ['**/*.{js,jsx}'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    '<rootDir>/coverage/',
+    '.config.js',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 25,
+      functions: 25,
+      lines: 25,
+      statements: 25,
+    },
+  },
 };
