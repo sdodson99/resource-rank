66f88741bac92d33befd2766bae0eb1e31dc6b57
Sean Dodson <dodsonsean@comcast.net>
2021-07-31 13:41:45 -0400

    RR-142: Update half rating star component

--- src/client/components/RatingStars/HalfRatingStar/HalfRatingStar.js
+++ src/client/components/RatingStars/HalfRatingStar/HalfRatingStar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './HalfRatingStar.module.css';
+import { StarHalfRounded } from '@material-ui/icons';
 
-const HalfRatingStar = () => (
+const HalfRatingStar = ({ size, color }) => (
   <div className={styles.HalfRatingStar} data-testid="HalfRatingStar">
-    HalfRatingStar Component
+    <StarHalfRounded htmlColor={color} style={{ fontSize: size }} />
   </div>
 );
 
-HalfRatingStar.propTypes = {};
+HalfRatingStar.propTypes = {
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
 
-HalfRatingStar.defaultProps = {};
+HalfRatingStar.defaultProps = {
+  size: 20,
+  color: 'black',
+};
 
 export default HalfRatingStar;
--- src/client/components/RatingStars/HalfRatingStar/HalfRatingStar.test.js
+++ src/client/components/RatingStars/HalfRatingStar/HalfRatingStar.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import HalfRatingStar from './HalfRatingStar';
+import { createRenderer } from 'react-test-renderer/shallow';
 
 describe('<HalfRatingStar />', () => {
   test('it should mount', () => {
@@ -10,5 +11,25 @@ describe('<HalfRatingStar />', () => {
     const halfRatingStar = screen.getByTestId('HalfRatingStar');
 
     expect(halfRatingStar).toBeInTheDocument();
+  });
+
+  it('should render correctly', () => {
+    const component = createRenderer().render(<HalfRatingStar />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correctly with custom size', () => {
+    const component = createRenderer().render(<HalfRatingStar size={100} />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render correctly with custom color', () => {
+    const component = createRenderer().render(
+      <HalfRatingStar color={'yellow'} />
+    );
+
+    expect(component).toMatchSnapshot();
   });
 });
