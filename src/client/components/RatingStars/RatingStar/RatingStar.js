1437cd3d6e657f99c84485559ea8ff54c1beb308
Sean Dodson <dodsonsean@comcast.net>
2021-07-31 20:59:11 -0400

    RR-142: Update rating star component

--- src/client/components/RatingStars/RatingStar/RatingStar.js
+++ src/client/components/RatingStars/RatingStar/RatingStar.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './RatingStar.module.css';
+import HalfRatingStar from '../HalfRatingStar/HalfRatingStar';
+import FullRatingStar from '../FullRatingStar/FullRatingStar';
+import EmptyRatingStar from '../EmptyRatingStar/EmptyRatingStar';
 
-const RatingStar = () => (
-  <div className={styles.RatingStar} data-testid="RatingStar">
-    RatingStar Component
-  </div>
-);
+const RatingStar = ({ fill, size, color }) => {
+  const getRatingStarForFill = () => {
+    const isHalfStar = fill >= 0.3 && fill <= 0.7;
+    if (isHalfStar) {
+      return <HalfRatingStar size={size} color={color} />;
+    }
 
-RatingStar.propTypes = {};
+    const isFullStar = fill > 0.7;
+    if (isFullStar) {
+      return <FullRatingStar size={size} color={color} />;
+    }
 
-RatingStar.defaultProps = {};
+    return <EmptyRatingStar size={size} />;
+  };
+
+  return (
+    <div className={styles.RatingStar} data-testid="RatingStar">
+      {getRatingStarForFill()}
+    </div>
+  );
+};
+
+RatingStar.propTypes = {
+  fill: PropTypes.number,
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+RatingStar.defaultProps = {
+  size: 20,
+  color: 'black',
+};
 
 export default RatingStar;
--- src/client/components/RatingStars/RatingStar/RatingStar.stories.js
+++ src/client/components/RatingStars/RatingStar/RatingStar.stories.js
@@ -10,3 +10,34 @@ const Template = (args) => <RatingStar {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const Color = Template.bind({});
+Color.args = {
+  color: 'orange',
+  fill: 1,
+};
+
+export const Size = Template.bind({});
+Size.args = {
+  ...Color.args,
+  size: '100',
+  fill: 1,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  ...Color.args,
+  fill: 0,
+};
+
+export const Half = Template.bind({});
+Half.args = {
+  ...Color.args,
+  fill: 0.5,
+};
+
+export const Full = Template.bind({});
+Full.args = {
+  ...Color.args,
+  fill: 1,
+};
