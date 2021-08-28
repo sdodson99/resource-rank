85347c54b1ae4441e865f3216374e7b40e0f174c
Sean Dodson <dodsonsean@comcast.net>
2021-08-28 13:31:34 -0400

    RR-196: Add route loading indicator

--- src/client/pages/__tests__/_app.test.js
+++ src/client/pages/__tests__/_app.test.js
@@ -2,13 +2,18 @@ import React from 'react';
 import App from '../_app.page';
 import { createRenderer } from 'react-test-renderer/shallow';
 import { useRouter } from 'next/router';
+import useRouteLoadingEffect from '@/hooks/routing/use-route-loading-effect';
+import { render } from '@testing-library/react';
 
 jest.mock('next/router');
+jest.mock('@/hooks/routing/use-route-loading-effect');
 
 describe('<App />', () => {
   let Component;
 
   beforeEach(() => {
+    useRouter.mockReturnValue({ asPath: '/path' });
+
     Component = function MockComponent() {
       return <div>Test component</div>;
     };
@@ -16,13 +21,18 @@ describe('<App />', () => {
 
   afterEach(() => {
     useRouter.mockReset();
+    useRouteLoadingEffect.mockReset();
   });
 
   it('should render correctly', () => {
-    useRouter.mockReturnValue({ asPath: '/path' });
-
     const page = createRenderer().render(<App Component={Component} />);
 
     expect(page).toMatchSnapshot();
+  });
+
+  it('should use route loading effect', () => {
+    render(<App Component={Component} />);
+
+    expect(useRouteLoadingEffect).toBeCalled();
   });
 });
