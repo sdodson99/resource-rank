867503081908480f203fbf5fb36793660dc5039c
Sean Dodson <dodsonsean@comcast.net>
2021-08-29 16:28:00 -0400

    RR-198: Update hero section

--- src/client/components/Hero/Hero.js
+++ src/client/components/Hero/Hero.js
@@ -1,59 +1,40 @@
-import React, { useState } from 'react';
+import Link from 'next/link';
+import React from 'react';
 import styles from './Hero.module.css';
-import { useRouter } from 'next/router';
+import { ArrowForward } from '@material-ui/icons';
 
-const Hero = () => {
-  const [searchTopicsQuery, setSearchTopicsQuery] = useState('');
-  const router = useRouter();
-
-  const onSearchTopicsQueryChange = (e) => {
-    const value = e.target.value;
-
-    setSearchTopicsQuery(value);
-  };
-
-  const onSearchTopics = () => {
-    router.push(`/topics?q=${searchTopicsQuery}`);
-  };
-
-  return (
-    <div className={styles.Hero} data-testid="Hero">
-      <div className={`content-container ${styles.Content}`}>
-        <div className="text-7xl">What are you planning to learn?</div>
-        <div className={styles.Search}>
-          <input
-            type="text"
-            className="flex-grow block form-control-lg"
-            placeholder="Search topics..."
-            value={searchTopicsQuery}
-            onChange={onSearchTopicsQueryChange}
-          />
-          <button
-            className="mt-5 sm:mt-0 sm:ml-3 btn btn-primary"
-            onClick={onSearchTopics}
-          >
-            Search
-          </button>
+const Hero = () => (
+  <div className={styles.Hero} data-testid="Hero">
+    <div className="content-container md:flex items-center justify-between">
+      <div className={styles.PromptSection}>
+        <div className={styles.Prompt}>
+          <span className={styles.Emphasis}>Level up</span> your{' '}
+          <span className={styles.Emphasis}>learning</span>.
         </div>
-        <div className="mt-24">
-          <div className="text-2xl">
-            <strong>Resource Rank</strong> helps you find the{' '}
-            <strong>best</strong> resources for your learning journey.
-          </div>
-          <div className="mt-8 flex justify-center">
-            <img
-              src="/img/logo.svg"
-              alt="Resource Rank Logo"
-              height="100"
-              width="100"
-            />
-          </div>
+        <div className={styles.SubPrompt}>
+          <span className={styles.Emphasis}>Resource Rank</span> helps you find
+          the <span className={styles.Emphasis}>best</span> resources for your{' '}
+          <span className={styles.Emphasis}>learning journey</span>.
+        </div>
+        <div className="mt-12 flex justify-center md:justify-start">
+          <Link href="/topics">
+            <a className="btn btn-primary flex items-center">
+              Explore Topics
+              <span className="ml-2">
+                <ArrowForward />
+              </span>
+            </a>
+          </Link>
         </div>
       </div>
-      <div className={styles.ColorOverlay}></div>
+      <div className={styles.IllustrationSection}>
+        <div className="flex items-center justify-center">
+          <img className={styles.Illustration} src="/img/hero.svg" />
+        </div>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 Hero.propTypes = {};
 
