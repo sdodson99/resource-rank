c27e07aefb385b77bca9ddba571cb961169731ef
Sean Dodson <dodsonsean@comcast.net>
2021-09-19 00:13:08 -0400

    RR-246: Fix storybook mocks

--- src/client/components/Link/Link.stories.js
+++ src/client/components/Link/Link.stories.js
@@ -1,18 +1,9 @@
-import { MockProvider } from '@/hooks/use-mock-context';
 import React from 'react';
 import Link from './Link';
 
 export default {
   component: Link,
   title: 'Components/Link',
-  decorators: [
-    (Story) => {
-      return;
-      <MockProvider>
-        <Story />
-      </MockProvider>;
-    },
-  ],
 };
 
 const Template = (args) => <Link {...args} />;
